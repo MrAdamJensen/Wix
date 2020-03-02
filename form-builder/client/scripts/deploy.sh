e606f5e79e4a4e9a97dc021143f20bacfefd8798
Yehonatan Peleg <pelegye@post.bgu.ac.il>
2020-03-02 22:29:33 +0200

    created server and updated client deploy to move all client files to server

--- form-builder/client/scripts/deploy.sh
+++ form-builder/client/scripts/deploy.sh
@@ -14,8 +14,20 @@ uglify -s submit_app_bundle.js -o __deployme/submit_app_bundle.js
 cssshrink bundle.css > __deployme/bundle.css
 
 # copy html and images
-cp index.html __deployme/index.html
+cp form_builder_app.html __deployme/form_builder_app.html
+cp submissions_app.html __deployme/submissions_app.html
+cp submit_app.html __deployme/submit_app.html
 cp -r images/ __deployme/images/
+
+# move deploy to server
+cp __deployme/form_builder_app.html ../server/form_builder_server/form_builder/templates/form_builder_app.html
+cp __deployme/submissions_app.html ../server/form_builder_server/form_builder/templates/submissions_app.html
+cp __deployme/submit_app.html ../server/form_builder_server/form_builder/templates/submit_app.html
+cp __deployme/form_builder_app_bundle.js ../server/form_builder_server/form_builder/static/js/form_builder_app_bundle.js
+cp __deployme/submissions_app_bundle.js ../server/form_builder_server/form_builder/static/js/submissions_app_bundle.js
+cp __deployme/submit_app_bundle.js ../server/form_builder_server/form_builder/static/js/submit_app_bundle.js
+cp __deployme/bundle.css ../server/form_builder_server/form_builder/static/css/bundle.css
+cp __deployme/images/ ../server/form_builder_server/form_builder/static/images
 
 # done
 date; echo;
